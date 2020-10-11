5db09c55daa5683d5f5e6576ed290b30a34ce67e
John Karima <karimajohn24@gmail.com>
2020-10-11 12:47:02 +0300

    create image and tag model

--- pics/models.py
+++ pics/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_name = models.CharField(max_length =30)
+    image_description = models.TextField()
+    
+
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
