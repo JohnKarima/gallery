88799f7999edb123d5529b2cceba6f0edb8460f6
John Karima <karimajohn24@gmail.com>
2020-10-11 13:01:12 +0300

    create and run initial tests for editor

--- pics/models.py
+++ pics/models.py
@@ -1,13 +1,33 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
-class Image(models.Model):
-    image_name = models.CharField(max_length =30)
-    image_description = models.TextField()
+
+class Editor(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length = 10, blank = True)
+
+    def __str__(self):
+        return self.first_name
+
+    def save_editor(self):
+        self.save()
     
+    class Meta:
+        ordering = ['first_name']
+
 
 class tags(models.Model):
     name = models.CharField(max_length =30)
 
     def __str__(self):
         return self.name
+
+class Image(models.Model):
+    image_name = models.CharField(max_length =30)
+    image_description = models.TextField()
+    editor = models.ForeignKey('Editor', on_delete=models.CASCADE, null=True, blank=True)
+    tags = models.ManyToManyField(tags)
+    pub_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
--- pics/tests.py
+++ pics/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
-
+from .models import Editor,Image,tags
+import datetime as dt
 # Create your tests here.
+
+class EditorTestClass(TestCase):
+
+    def setUp(self):
+        self.james= Editor(first_name = 'James', last_name = 'Muriuki', email = 'james"moringaschool.com')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.james, Editor))
+
+    def test_save_method(self):
+        self.james.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors)>0)
+
+class ImageTestClass(TestCase):
+    
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+        self.james.save_editor()
+
+        # Creating a new tag and saving it
+        self.new_tag = tags(name = 'testing')
+        self.new_tag.save()
+
+        self.new_image= Image(title = 'Test Image',post = 'This is a random test Post',editor = self.james)
+        self.new_image.save()
+
+        self.new_image.tags.add(self.new_tag)
+
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Image.objects.all().delete()
