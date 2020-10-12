91725b55bbed2db69ac6a80ff27e82a6cad55ec1
John Karima <karimajohn24@gmail.com>
2020-10-12 14:27:52 +0300

    testing app

--- pics/tests.py
+++ pics/tests.py
@@ -16,3 +16,45 @@ class ImageTestClass(TestCase):
 
     def test_instance(self):
         self.assertTrue(isinstance(self.coolpic, Image))
+
+    def test_save_image(self):
+        self.coolpic.save_image()
+        pics = Image.objects.all()
+        self.assertTrue(len(pics) > 0)
+
+    def test_delete_image(self):
+        self.coolpic.delete_image()
+        pics = Image.objects.all()
+        self.assertTrue(len(pics) == 0)
+
+   
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.fiction= Category(category='fiction')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.fiction,Category))
+
+    def tearDown(self):
+        Category.objects.all().delete()
+
+    def test_delete_category(self):
+        self.fiction.delete_category('fiction')
+        category = Category.objects.all()
+        self.assertTrue(len(category)==0)
+
+class LocationTestClass(TestCase):
+
+    def setUp(self):
+        self.africa= Location(location='africa')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.africa,Location))
+
+    def tearDown(self):
+        Location.objects.all().delete()
+
+    def test_delete_location(self):
+        self.africa.delete_location('africa')
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)==0)
